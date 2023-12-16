0b881920e1726898de319149bf1c5077b39dfc25
Dương Võ Thiên Bảo <135791389+Afihu@users.noreply.github.com>
2023-12-16 18:35:57 +0700

    Implements functions in sudoku.c

--- src/hidden_singles.c
+++ src/hidden_singles.c
@@ -1,6 +1,68 @@
+// hidden_singles.c
 #include "hidden_singles.h"
 
+// Find hidden single values (the value that is unique among all the candidates for a row, a column, or a box)
+int find_hidden_single_values(Cell **p_cells, int *hidden_single_values)
+{
+    // Implement the logic for finding hidden single values in a row, column, or box
+    // Update the hidden_single_values array with the found values
+    // Return the number of hidden single values found
+    // ...
+    return 0; // Placeholder, update accordingly
+}
+
+// Find hidden single cells in a row, in a column, or in a box
+void find_hidden_single(Cell **p_cells, HiddenSingle *p_hidden_singles, int *p_counter)
+{
+    // Implement the logic for finding hidden single cells in a row, column, or box
+    // Update the p_hidden_singles array and increment the counter for each found hidden single
+    // ...
+}
+
+// Find hidden single cells in a board, return the number of hidden single cells
 int hidden_singles(SudokuBoard *p_board)
 {
-    return 0;
+    int total_hidden_singles = 0;
+
+    // Iterate through rows
+    for (int i = 0; i < BOARD_SIZE; ++i)
+    {
+        int hidden_single_values[BOARD_SIZE];
+        int hidden_single_count = find_hidden_single_values(p_board->p_rows[i], hidden_single_values);
+
+        if (hidden_single_count > 0)
+        {
+            // Handle or store the hidden single values as needed
+            total_hidden_singles += hidden_single_count;
+        }
+    }
+
+    // Iterate through columns
+    for (int j = 0; j < BOARD_SIZE; ++j)
+    {
+        int hidden_single_values[BOARD_SIZE];
+        int hidden_single_count = find_hidden_single_values(p_board->p_cols[j], hidden_single_values);
+
+        if (hidden_single_count > 0)
+        {
+            // Handle or store the hidden single values as needed
+            total_hidden_singles += hidden_single_count;
+        }
+    }
+
+    // Iterate through boxes
+    for (int k = 0; k < BOARD_SIZE; ++k)
+    {
+        int hidden_single_values[BOARD_SIZE];
+        int hidden_single_count = find_hidden_single_values(p_board->p_boxes[k], hidden_single_values);
+
+        if (hidden_single_count > 0)
+        {
+            // Handle or store the hidden single values as needed
+            total_hidden_singles += hidden_single_count;
+        }
+    }
+
+    // Return the total number of hidden single cells found
+    return total_hidden_singles;
 }
--- src/sudoku.c
+++ src/sudoku.c
@@ -1,10 +1,109 @@
 #include "sudoku.h"
-
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "hidden_singles.h"
+
+// sudoku.c
+#include "sudoku.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void init_sudoku(SudokuBoard *p_board) {
+    // Initialize solved_counter to 0
+    p_board->solved_counter = 0;
+
+    // Allocate memory for the data array (9x9 grid of cells)
+    p_board->data = (Cell**)malloc(BOARD_SIZE * sizeof(Cell*));
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        p_board->data[i] = (Cell*)malloc(BOARD_SIZE * sizeof(Cell));
+    }
+
+    // Initialize other pointers to NULL
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        p_board->p_rows[i] = NULL;
+        p_board->p_cols[i] = NULL;
+        p_board->p_boxes[i] = NULL;
+    }
+
+    // Initialize solved_cells array to NULL
+    for (int i = 0; i < BOARD_SIZE * BOARD_SIZE; i++) {
+        p_board->solved_cells[i] = NULL;
+    }
+}
+
+void load_sudoku(SudokuBoard *p_board, char *input_text) {
+    // Implement your code to load a Sudoku puzzle from input_text
+    // This function should update the data array in the SudokuBoard structure
+}
+
+bool apply_constraint(Cell **p_cells, int value) {
+    // Implement your code to apply a constraint to the given cells
+    // This function should return true if the constraint is successfully applied
+}
+
+bool is_in_list(Cell **p_array, int size, Cell *p) {
+    // Implement your code to check if a cell is in the given list
+    // This function should return true if the cell is in the list
+}
+
+void print_candidate_num(SudokuBoard *p_board) {
+    // Implement your code to print the number of candidates for each cell
+}
+
+void print_solution(SudokuBoard *p_board) {
+    // Implement your code to print the solved Sudoku solution
+}
+
+void set_candidate(Cell *cell, int value) {
+    // Implement your code to set a candidate for the given cell
+}
+
+void unset_candidate(Cell *cell, int value) {
+    // Implement your code to unset a candidate for the given cell
+}
+
+void set_candidates(Cell *cell, int *candidates, int size) {
+    // Implement your code to set candidates for the given cell
+}
+
+int *get_candidates(Cell *cell) {
+    // Implement your code to get the candidates for the given cell
+    // This function should return a dynamically allocated array of candidates
+}
+
+int check_solved_cells(SudokuBoard *p_board, Cell ***p_solved_cells) {
+    // Implement your code to check for solved cells and update the solved_cells array
+    // This function should return the number of newly solved cells
+}
+
+bool show_possible(SudokuBoard *p_board, Cell **p_solved_cells, int counter) {
+    // Implement your code to show possible solutions for unsolved cells
+    // This function should return true if possible solutions are shown
+}
+
+void free_sudoku(SudokuBoard *p_board) {
+    // Free the data array (9x9 grid of cells)
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        free(p_board->data[i]);
+    }
+    free(p_board->data);
+
+    // Free other pointers
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        free(p_board->p_rows[i]);
+        free(p_board->p_cols[i]);
+        free(p_board->p_boxes[i]);
+    }
+
+    // Free solved_cells array
+    for (int i = 0; i < p_board->solved_counter; i++) {
+        free(p_board->solved_cells[i]);
+    }
+
+    // Finally, free the SudokuBoard structure itself
+    free(p_board);
+}
 
 int main(int argc, char **argv)
 {
@@ -14,7 +113,7 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    SudokuBoard *board = malloc(sizeof(SudokuBoard));
+    SudokuBoard *board = (SudokuBoard*)malloc(sizeof(SudokuBoard));
     init_sudoku(board);
     load_sudoku(board, argv[1]);
 
@@ -23,23 +122,33 @@ int main(int argc, char **argv)
     while (board->solved_counter < BOARD_SIZE * BOARD_SIZE)
     {
         solved_counter = check_solved_cells(board, &p_solved_cells);
-        // printf("check_solved_cells %d\n", solved_counter);
+        printf("check_solved_cells %d\n", solved_counter);
         if (show_possible(board, p_solved_cells, solved_counter))
         {
-            // printf("show_possible -> Yes\n");
+            printf("show_possible -> Yes\n");
             continue;
         }
-        // solved_counter = hidden_singles(board);
-        // if (solved_counter)
-        // {
-        //     printf("hidden_singles %d\n", solved_counter);
-        //     continue;
-        // }
+        // Uncomment the line below to activate the hidden_singles function
+        solved_counter = hidden_singles(board);
+        if (solved_counter)
+        {
+            printf("hidden_singles %d\n", solved_counter);
+            continue;
+        }
     }
+
+    // Call hidden_singles and handle the return value
+    // int hidden_singles_count = hidden_singles(board);
+    // if (hidden_singles_count > 0)
+    // {
+    //     printf("hidden_singles %d\n", hidden_singles_count);
+    //     // Additional logic if needed
+    // }
+
     print_solution(board);
 
-    // clean up
+    // Clean up (free_sudoku already frees the board)
     free_sudoku(board);
-    free(board);
+
     return 0;
 }
